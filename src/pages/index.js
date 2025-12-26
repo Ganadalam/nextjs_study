b3b1aa82ac41dc1be10c52da35bbf8ae445097ba
JEN023__ <lunar_halo_@naver.com>
2025-12-26 18:12:35 +0900

    init: initial

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import NavBar from "@/components/NavBar";
+import { useState } from "react";
+
+export default function Home() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <NavBar />
+      <h1> Hello </h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+    </div>
+  );
+}
